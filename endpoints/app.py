bf0d5bd0d9ff3cf90adaccd8f81a6d3984a4223e
Seth Coleman <84861800+SethColeman50@users.noreply.github.com>
2025-03-25 13:53:55 -0400

    merged a stashed thing wi

--- endpoints/app.py
+++ endpoints/app.py
@@ -1,4 +1,6 @@
+
 from queries.query import search
+import json
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -34,13 +36,55 @@ def lambda_handler(event, context):
 
     if 'queryStringParameters' not in event or event['queryStringParameters'] == None or PARAMETER_NAME not in event['queryStringParameters']:
         response["statusCode"] = 400
-        response['body'] = f"Missing query parameter '{PARAMETER_NAME}'"
+        response['body'] = f"Bad request: Missing query parameter '{PARAMETER_NAME}'"
         return response
     
-    search_term =  event['queryStringParameters'][PARAMETER_NAME]
+    if event['header'] == None:
+        response["statusCode"] = 400
+        response['body'] = "Bad request: missing header"
+        return response
+    
+    if event['body'] == None:
+        response["statusCode"] = 400
+        response['body'] = "Bad request: missing body"
+        return response
+    
+    query_parameters = event['queryStringParameters']
+    header = event['header']
+    body = json.dumps(event['body'])
+
+    
+    input_json = {
+        "searchTerm": query_parameters["searchTerm"],
+        "pageNumber": query_parameters["pageNumber"],
+        "refreshResult": query_parameters["refreshResults"],
+        "sessionID": header["sessionID"],
+        "sortParams": body["sortParams"],
+        "filterParams": body["filterParams"]
+    }
 
     try:
-        body = search(search_term)
+        # body = search(search_term)
+        body = json.dumps([
+            {
+                "docketId": "BIS-2024-0053",
+                "numComments": "70/100",
+                "docketTitle": "Public Briefing on Revisions to Space Related Export Controls Under Export Administration Regulations and International Traffic in Arms Regulations",
+                "matchQuality": ".85 "
+            },
+            {
+                "docketId": "DOS-2022-0004",
+                "numComments": "24/100",
+                "docketTitle": "International Traffic in Arms Regulations: Consolidation and Restructuring of Purposes and Definitions",
+                "matchQuality": ".6"
+            },
+            {
+                "docketId": "DOS-2010-0194",
+                "numComments": "2/50",
+                "docketTitle": "2008 - Amendment to the International Arms Traffic in Arms Regulations: Eritrea",
+                "matchQuality": ".2"
+            }
+        ])
     except Exception as e:
         print(f"Exception in database query: {e}")
         response["statusCode"] = 500
